c16e084346c9868f2818eb6e1f0ae974036156c0
guozy <guozystc@outlook.com>
2025-05-07 20:53:52 +0800

    add

--- styles.js
+++ styles.js
@@ -7,3 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
     navMenu.classList.toggle("active");
   });
 });
+
+//回到顶部
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("back-to-top");
+
+  window.addEventListener("scroll", function () {
+    btn.style.display = window.scrollY > 300 ? "block" : "none";
+  });
+
+  btn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
